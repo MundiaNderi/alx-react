60520649a362ab99f83994cc1b46ae00ae12d549
MundiaNderi <sarahngima77@gmail.com>
2023-08-21 23:52:18 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,7 +1,17 @@
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
+import './main.css'; // Import your CSS file
 
 let counter = 0;
+
+$("<div id='logo'></div>").prependTo('body'); // Add a div with id 'logo' at the top of the body
+$("#logo").css({
+  width: '200px',
+  height: '200px',
+  backgroundImage: 'url(assets/holberton-logo.jpg)',
+  backgroundSize: 'cover', // Set the background image size to cover the div
+});
+
 
 $("<div id='logo'> </div>").appendTo('body');
 $("<p>Holberton Dashboard</p>").appendTo('body');
