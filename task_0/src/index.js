77adaac9295bb714618aed3a422fad1f0b159fdc
MundiaNderi <sarahngima77@gmail.com>
2023-08-21 17:38:01 +0300

    Fixing directories

--- task_0/src/index.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-
-$(document).ready(function () {
-    $('body').append('<p>Paragraph 1 <p>');
-    $('body').append('<p>Paragraph 2 <p>');
-    $('body').append('<p>Paragraph 3 <p>');
-});
