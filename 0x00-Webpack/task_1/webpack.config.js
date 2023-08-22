b3213445f443cd0bf7fd34520e1f3c271a4b57be
MundiaNderi <sarahngima77@gmail.com>
2023-08-22 11:41:55 +0300

    Fixing bundle.js

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production', // Set Webpack mode to production
-  entry: './src/js/dashboard_main.js',
+  mode: 'production', 
+  entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
