5302609cfc99a93a62c446bba3724501da0c9e01
MundiaNderi <sarahngima77@gmail.com>
2023-08-25 13:23:43 +0300

    Configure Enzyme with the React 18 adapter

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-18';
+
+// Configure Enzyme with the React 18 adapter
+configure({ adapter: new Adapter() });
