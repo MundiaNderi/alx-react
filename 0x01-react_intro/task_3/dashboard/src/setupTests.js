2697846c39e4202884b03f05484c31a28282c386
MundiaNderi <sarahngima77@gmail.com>
2023-08-25 17:00:23 +0300

    Create React tests

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -2,9 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-18';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-16';
 
-// Configure Enzyme with the React 18 adapter
+// Configure Enzyme with the React 16 adapter
 configure({ adapter: new Adapter() });
