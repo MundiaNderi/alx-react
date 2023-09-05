f045e62419b4b45700c761d2d38608346eff2feb
MundiaNderi <sarahngima77@gmail.com>
2023-09-05 23:21:21 +0300

    Add a test

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notificationsimport { isForInStatement } from '@babel/types';
+ './Notifications';
+
+describe('Notifications component', () => {
+    it('should not rerender when props are updated with the same list', () => {
+        const notifications = [
+            {
+                id: 1,
+                type: 'info',
+                value: 'Notification 1'
+            },
+            {
+                id: 2,
+                type: 'warning',
+                value: 'Notification 2'
+            },
+        ];
+
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={notifications} />);
+
+        const shouldComponentUpdateSpy = jest.spyon(
+            Notifications.proptype,
+            'shouldComponentUpdateSpy'
+        );
+
+        // Update props with the same list
+        wrapper.setProps({ displayDrawer: false, listNotifications: notifications});
+
+        // Expect shouldComponentUpdate not to have been called
+        expect(shouldComponentUpdateSpy).not.toHaveBeenCalled();
+
+        shouldComponentUpdateSpy.mockRestore();
+    });
+
+    it('should rerender when props are updated with a longer list', () => {
+        const initialNotifications = [
+            {
+            id: 1,
+            types: 'info',
+            value: 'Notification 1'
+        },
+    ];
+
+    const updatedNotifications = [
+        {
+            id: 1,
+            type: 'info',
+            value: 'Notification 1',
+        },
+        {
+            id: 2,
+            type: 'warning',
+            value: 'Notification 2'
+        },
+    ];
+
+    const wrapper = shallow (
+        <Notifications displayDrawer={true} listNotifications={initialNotifications} />
+    );
+
+    const shouldComponentUpdateSpy = jest.spyOn(
+        Notifications.prototype,
+        'shouldComponentUpdate'
+    );
+
+    // Update props with a longer list
+    wrapper.setProps({ displayDrawer: false, listNotifications: updatedNotifications });
+
+    // Expect shouldComponentUpdate to have been called
+    expect(shouldComponentUpdateSpy).toHaveBeenCalled();
+
+    shouldComponentUpdateSpy.mockRestore();
+
+    });
+});
