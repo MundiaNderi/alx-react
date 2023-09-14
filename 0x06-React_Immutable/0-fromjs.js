c77af0fe7497b8bb86936a50dd6066351399862e
MundiaNderi <sarahngima77@gmail.com>
2023-09-14 13:31:04 +0300

    Fixing task 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,5 @@
-// Import the 'fromJS' function from the Immutable.js library
 import { fromJS } from 'immutable';
 
-// Define a function named 'getImmutableObject' that accepts an 'object' as a parameter
-// and converts it into an Immutable Map using the 'fromJS' function.
-export default function getImmutableObject(object) {
-    // Call the 'fromJS' function to convert the 'object' into an Immutable Map
-    return fromJS(object);
-}
+const getImmutableObject= (object) => fromJS(object);
+
+export default getImmutableObject;
