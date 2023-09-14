632fafbed4270bc7cdeb36bfa55311f6f1c2f3c1
MundiaNderi <sarahngima77@gmail.com>
2023-09-14 12:24:35 +0300

    Converting into an Immutable object using fromJS

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,9 @@
+// Import the Immutable.js library
+const Immutable = require('Immutable');
+
+// Define the getImmutableFuction
+function getImmutableFuction(object) {
+    // Convert the input object to an immutable Map
+    const immutableMap = Immutable.fromJS(object);
+    return immutableMap;
+}
