954207dc17bd27118d6ff2c24a226910068c4594
MundiaNderi <sarahngima77@gmail.com>
2023-09-15 10:58:33 +0300

    List and push

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,6 +1,11 @@
-import { fromJS } from 'immutable';
-import { get } from './node_modules/immutable/dist/immutable';
+import { List } from 'immutable'; // Import List from Immutable.js
 
-const getImmutableObject = (object) => fromJS(object);
+// Function to convert an array to an Immutable List
+export function getListObject(array) {
+    return List(array); // Convert the array to an Immutable List
+}
 
-export default getImmutableObject;
+// Function to add an element to an Immutable List
+export function addElementToList(list, element) {
+    return list.push(element); // Append the element to the list and return the updated list
+}
