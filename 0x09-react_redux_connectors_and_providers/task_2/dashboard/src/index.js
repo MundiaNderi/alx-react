7648f4e29d6fbb406c35b0d0b24cbfd333f59ef8
MundiaNderi <sarahngima77@gmail.com>
2023-10-02 13:32:58 +0300

    Connect LoginRequest to the App

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from 'redux';  // Import createStore from Redux
+import { createStore, applyMiddleware} from 'redux';  // Import createStore from Redux
 import { Provider } from 'react-redux';  // Import Provider from react-redux
 import App from './App/App';
 import uiReducer from './reducers/uiReducer'; // Import your uiReducer;
+import thunk from "redux-thunk";
 
-// Create the Redux store with your uiReducer
-const store = createStore(uiReducer);
+// Create the Redux store with your uiReducer and apply the thunk middleware
+const store = createStore(uiReducer, applyMiddleware(thunk));
 
 // Wrap your App component with the Provider and pass the store as a prop
 ReactDOM.render(
