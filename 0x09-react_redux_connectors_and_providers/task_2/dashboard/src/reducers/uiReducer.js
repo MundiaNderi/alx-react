10c02e77806285577973d5f2ff8e41b670523d3e
MundiaNderi <sarahngima77@gmail.com>
2023-10-03 10:35:06 +0300

    Modify the uiReducer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -9,11 +9,11 @@ import {
     LOGOUT,
 } from '../actions/uiActionTypes';
 
-
+// Initial state with the user set to null
 const initialState = Map({
     isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: Map(),
+    isUserLoggedIn: false, // Track user login status
+    user: null, // Store user data
 });
 
 const uiReducer = (state = initialState, action) => {
@@ -25,11 +25,15 @@ const uiReducer = (state = initialState, action) => {
             return state.set('isNotificationDrawerVisible', false);
 
         case LOGIN_SUCCESS:
-            return state.set('isUserLoggedIn', true);
+            return state
+            .set('isUserLoggedIn', true) // Set the user data from the action
+            .set('user', action.user) // Set the user data from the action
 
         case LOGIN_FAILURE:
         case LOGOUT:
-            return state.set('isUserLoggedIn', false);
+            return state
+            .set('isUserLoggedIn', false) // Set user login status to false
+            .set('user', null) // Set the user data to null
 
         default:
             return state;
