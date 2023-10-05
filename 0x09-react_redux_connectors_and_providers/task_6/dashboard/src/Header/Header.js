a50735b7e52f7ca65acff66e9e723242c2b2db80
MundiaNderi <sarahngima77@gmail.com>
2023-10-05 23:34:51 +0300

    Selectors

---
+++ 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/Header/Header.js
@@ -0,0 +1,52 @@
+import { connect } from "react-redux";
+import { logout } from '../actions/uiActionCreators';
+import logo from '../assets/';
+import { StyleSheet, css } from "aphrodite";
+
+
+// mapStateToProps function to map the user prop to the user within the redux state
+const mapStateToProps = (state) => ({
+    user: state.uiReducer.user,
+})
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: "1.4rem",
+        color: "#e0354b",
+        display: "flex",
+        alignItems: "center",
+        borderBottom: "3px solid #e0354b",
+        paddingTop: "200px",
+    },
+    logo: {
+        width: "200px",
+        height: "200px",
+    },
+    welcome: {
+        marginLeft: "auto", // Move the welcome text to the right
+    },
+});
+
+class Header extends React.Component {
+    render() {
+      const { user, logout } = this.props; // Get user and logout from props
+      return (
+        <div className={css(styles.header)}>
+          <img src={logo} className={css(styles.logo)} alt="logo" />
+          <h1>School Dashboard</h1>
+          {/* Conditional rendering based on isLoggedIn */}
+          {user.isLoggedIn && (
+            <div className={css(styles.welcome)}>
+              Welcome {user.email} (
+              <span id="logoutSection" onClick={logout}>
+                logout
+              </span>
+              )
+            </div>
+          )}
+        </div>
+      );
+    }
+  }
+  
+export default connect(mapStateToProps, { logout })(Header);
